fdc8513089ca7fa3372499b79a77a7f24b792ed4
MariuszKaminski <mkaminski10@gmail.com>
2023-01-25 23:31:46 +0000

    Finished add product functionality

--- products/views.py
+++ products/views.py
@@ -9,7 +9,7 @@ from .forms import ProductForm
 # Create your views here.
 
 def all_products(request):
-    """ A view to show individual product details """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
     query = None
@@ -31,17 +31,16 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
         if 'q' in request.GET:
-            query = request.GET['q']            
+            query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter search criteria!")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
@@ -60,7 +59,7 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
@@ -73,7 +72,17 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Add a product to the store """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
     template = 'products/add_product.html'
     context = {
         'form': form,
