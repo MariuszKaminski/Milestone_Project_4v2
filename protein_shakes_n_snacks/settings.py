32290c5bf0cd8d6eae4c7a4b67825d0fd0c515e5
MariuszKaminski <mkaminski10@gmail.com>
2023-01-26 23:10:15 +0000

    Removed secret key and set debug

--- protein_shakes_n_snacks/settings.py
+++ protein_shakes_n_snacks/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l2ue1aln3yw=f1wox(auwu@)2@1u8%o1ns1)_n%#=42v+^e6o0'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['protein-shakes-n-snacks.herokuapp.com', 'localhost']
 
